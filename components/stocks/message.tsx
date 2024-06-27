02a8de14bb077bde269a51b94adc5ee197157d3a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-06-27 19:54:30 +0100

    remove unnecessary stuff

--- components/stocks/message.tsx
+++ components/stocks/message.tsx
@@ -14,7 +14,7 @@ import { useStreamableText } from '@/lib/hooks/use-streamable-text'
 
 export function UserMessage({ children }: { children: React.ReactNode }) {
   return (
-    <div className="group relative flex items-start md:-ml-12">
+    <div className="group relative flex items-start md:-ml-12 bg-zinc-50/10 p-2">
       <div className="flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border bg-background shadow-sm">
         <IconUser />
       </div>
